1512909c76853691adf732f09680acc32a575367
Willy Samuel Paz Colque <willypaz243@gmail.com>
2025-04-27 20:22:12 -0400

    feat(Validators): Add new validators classes
    - Added `PersonalDataValidator`, `SchoolDataValidator`, and `SelectedAreaValidator` classes under the `/app/Validators` directory.
    - Updated validation rules and messages for each validator.

--- app/Http/Controllers/PersonSearchController.php
+++ app/Http/Controllers/PersonSearchController.php
@@ -2,15 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-use App\Models\AcademicTutors;
+
 use App\Models\PersonalData;
+
 class PersonSearchController extends Controller
 {
     public function searchStudent($ci)
     {
         return response()->json(PersonalData::where('ci', $ci)->first());
     }
-
-  
 }
