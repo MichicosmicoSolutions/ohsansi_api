404c4d6c5c2d27d67c17adec0a1d0c20ebfbed1b
Alan Benavides <bruno@coastt.org>
2025-06-10 23:44:22 -0400

    add: inscriptionId to boleta

--- app/Http/Controllers/OCRController.php
+++ app/Http/Controllers/OCRController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\BoletaDePago;
 use App\Enums\InscriptionStatus;
 use App\Models\Inscriptions;
+use Illuminate\Support\Facades\DB;
 
 class OCRController extends Controller
 {
@@ -18,10 +19,11 @@ class OCRController extends Controller
         $numero = $request->input('numero_orden_de_pago');
         $numeroLimpio = ltrim($numero, '0');
 
-      
+
+
         $boleta = BoletaDePago::where('numero_orden_de_pago', $numero)
-                    ->orWhere('numero_orden_de_pago', $numeroLimpio)
-                    ->first();
+            ->orWhere('numero_orden_de_pago', $numeroLimpio)
+            ->first();
 
         if (!$boleta) {
             return response()->json([
@@ -30,20 +32,35 @@ class OCRController extends Controller
             ], 404);
         }
 
-        $inscripcion = Inscriptions::where('boleta_de_pago_id', $boleta->id)->first();
+        $inscripcions = Inscriptions::where('boleta_de_pago_id', $boleta->id)->get();
 
-        if (!$inscripcion) {
+        if ($inscripcions->isEmpty()) {
             return response()->json([
                 'error' => 'No se encontró ninguna inscripción asociada a esa boleta de pago.',
                 'numero_recibido' => $numero
             ], 404);
         }
 
-        $inscripcion->status = InscriptionStatus::COMPLETED;
-        $inscripcion->save();
+        foreach ($inscripcions as $inscripcion) {
+            if ($inscripcion->status !== InscriptionStatus::PENDING) {
+                return response()->json([
+                    'error' => 'La inscripción ya ha sido procesada.',
+                    'numero_recibido' => $numero
+                ], 400);
+            }
+        }
+
+        DB::beginTransaction();
+
+        foreach ($inscripcions as $inscripcion) {
+            $inscripcion->status = InscriptionStatus::COMPLETED;
+            $inscripcion->save();
+        }
 
         $boleta->status = 'completed';
         $boleta->save();
+
+        DB::commit();
 
         return response()->json([
             'mensaje' => 'El estado de la inscripción y la boleta de pago han sido actualizados a COMPLETED.',
